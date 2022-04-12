083a98a6cec6374ca95e10cbe08d77cd08d4efc1
liusj <1182758611@qq.com>
2022-04-12 16:46:05 +0800

    date04012

---
+++ V13/src/main/java/com/webserver/http/HttpServletResponse.java
@@ -0,0 +1,124 @@
+package com.webserver.http;
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+
+public class HttpServletResponse {
+    private Socket socket;
+    //状态行相关信息
+    private int s1 = 200;
+    private String s2 = "OK";
+    //响应头有相关信息
+    private Map<String, String> headers = new HashMap<>();
+    //响应正文相关信息
+    private File contentFile;
+
+    public HttpServletResponse(Socket socket) {
+        this.socket = socket;
+    }
+
+    /**
+     * 将当前响应对象内容按照标准的HTTP响应格式发送给客户端
+     */
+    public void response() throws IOException {
+        //3.1发送状态行
+        sendStatusLine();
+        //3.2发送响应头
+        sendHeaders();
+        //3.3发送响应正文(index.html页面的数据)
+        sendContent();
+
+    }
+
+    public void sendStatusLine() throws IOException {
+        println("HTTP/1.1 " + s1 + " " + s2);
+    }
+
+    public void sendHeaders() throws IOException {
+//        println("Content-Type: text/html");
+//        println("Content-Length: " + contentFile.length());
+        Set<Map.Entry<String, String>> entrySet = headers.entrySet();
+        for (Map.Entry<String, String> e : entrySet) {
+            println(e.getKey() + ": " + e.getValue());
+        }
+        println("");
+    }
+
+    public void sendContent() throws IOException {
+        OutputStream out = socket.getOutputStream();
+        FileInputStream fis = new FileInputStream(contentFile);
+        int len;
+        byte[] data = new byte[1024 * 10];
+        while ((len = fis.read(data)) != -1) {
+            out.write(data, 0, len);
+        }
+
+    }
+
+    private void println(String line) throws IOException {
+        OutputStream out = socket.getOutputStream();
+        byte[] data = line.getBytes(StandardCharsets.ISO_8859_1);
+        out.write(data);
+        out.write(13);
+        out.write(10);
+    }
+
+    public int getS1() {
+        return s1;
+    }
+
+    public void setS1(int s1) {
+        this.s1 = s1;
+    }
+
+    public String getS2() {
+        return s2;
+    }
+
+    public void setS2(String s2) {
+        this.s2 = s2;
+    }
+
+    public File getContentFile() {
+        return contentFile;
+    }
+
+    public void setContentFile(File contentFile) {
+        this.contentFile = contentFile;
+        String contentType = null;
+        try {
+            contentType = Files.probeContentType(contentFile.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /*当我们无法根据正文文件确定类型是,则不发送Content-Type
+        在HTTP协议中档响应包含正文,但是不包含Content-Type时,则表示让浏览器
+        自行判断响应正文内容
+         */
+        if (contentType != null) {
+            addHeader("Content-Type", contentType);
+            addHeader("Content-Length", contentFile.length() + "");
+        }
+    }
+
+    /**
+     * 添加一个要发送的响应头
+     *
+     * @param name
+     * @param value
+     */
+    public void addHeader(String name, String value) {
+        //将响应头的名字和值以key-value形式存入headers这个Map中
+        headers.put(name, value);
+    }
+}
