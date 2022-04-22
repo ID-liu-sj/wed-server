bb0e5b18772cecb3de25af4862a355df70daa911
liusj <1182758611@qq.com>
2022-04-22 21:26:08 +0800

    date0420

--- V26/src/main/java/com/webserver/controller/ToolsController.java
+++ V26/src/main/java/com/webserver/controller/ToolsController.java
@@ -2,14 +2,22 @@ package com.webserver.controller;
 
 import com.webserver.annotation.Controller;
 import com.webserver.annotation.RequestMapping;
-import com.webserver.core.DispatcherServlet;
+
 import com.webserver.http.HttpServletRequest;
 import com.webserver.http.HttpServletResponse;
 import qrcode.QRCodeUtil;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class ToolsController {
@@ -25,14 +33,46 @@ public class ToolsController {
         }
     }
 
+    @RequestMapping("/myweb/random.jpg")
+    public void createRandomImage(HttpServletRequest request, HttpServletResponse response) {
+        //验证码模式
+        //1创建一张空白图片,并且指定宽高
+        BufferedImage image = new BufferedImage(70, 30, BufferedImage.TYPE_3BYTE_BGR);
+
+        Graphics g = image.getGraphics();
+        String line = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM0987654321";
+        Random random = new Random();
+        Color bgcolor = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+        g.setColor(bgcolor);
+        g.fillRect(0, 0, 70, 30);
+
+        for (int i = 0; i < 4; i++) {
+            String str = line.charAt(random.nextInt(line.length())) + "";
+            Color color = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+            g.setColor(color);
+            g.setFont(new Font(null, Font.BOLD, 20));
+            g.drawString(str, i * 15 + 5, 18 + random.nextInt(11) - 5);
+        }
+
+        //随机生成4条干扰线
+        for (int i = 0; i < 4; i++) {
+            Color color = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+            g.drawLine(random.nextInt(71), random.nextInt(31), random.nextInt(71), random.nextInt(31));
+
+        }
+
+
+        try {
+            ImageIO.write(image, "jpg", response.getOutputStream());
+            response.setContentType("image/jpeg");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     public static void main(String[] args) throws URISyntaxException {
-
-        File fileDir = new File(DispatcherServlet.class.getClassLoader().getResource("./com/webserver/controller").toURI());
-
-        System.out.println(fileDir);
-
-
 
         /*String message = "http://www.bilibili.com";
         try {
@@ -48,5 +88,44 @@ public class ToolsController {
         } catch (Exception e) {
             e.printStackTrace();
         }*/
+
+        //验证码模式
+        //1创建一张空白图片,并且指定宽高
+        BufferedImage image = new BufferedImage(70, 30, BufferedImage.TYPE_3BYTE_BGR);
+
+        Graphics g = image.getGraphics();
+        String line = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM0987654321";
+        Random random = new Random();
+        Color bgcolor = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+        g.setColor(bgcolor);
+        g.fillRect(0, 0, 70, 30);
+
+        for (int i = 0; i < 4; i++) {
+            String str = line.charAt(random.nextInt(line.length())) + "";
+            Color color = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+            g.setColor(color);
+            g.setFont(new Font(null, Font.BOLD, 20));
+            g.drawString(str, i * 15 + 5, 18 + random.nextInt(11) - 5);
+        }
+
+        //随机生成4条干扰线
+        for (int i = 0; i < 4; i++) {
+            Color color = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+            g.drawLine(random.nextInt(71), random.nextInt(31), random.nextInt(71), random.nextInt(31));
+
+        }
+
+
+        try {
+            ImageIO.write(image, "jpg", new FileOutputStream("./random.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+//        List<String> list = new ArrayList<>();
+//        list.get(1);
+
+
     }
 }
